116a8dcdb302de9f7bb00566f2d92329295168f9
George Baronheid <baronheid.george@gmail.com>
2024-03-20 22:40:59 -0300

    fix: build|

--- backend/internal/service/timekeeping_service.go
+++ backend/internal/service/timekeeping_service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"context"
+	"github.com/SOAT1StackGoLang/Hackaton/internal/service/models"
+	"github.com/SOAT1StackGoLang/Hackaton/internal/service/persistence"
+)
+
+type tS struct {
+	tR *persistence.TimekeepingRepository
+}
+
+func (t tS) InsertEntry(ctx context.Context, in *models.Entry) (*models.Entry, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewTimekeepingService(persistence *persistence.TimekeepingRepository) TimekeepingService {
+	return &tS{
+		tR: persistence,
+	}
+}
