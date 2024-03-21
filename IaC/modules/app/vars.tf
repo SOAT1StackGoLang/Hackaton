94f8125ee211ada88977a20c65ce4f51298a83f0
Lucas O Arruda <lucas.o.arruda@live.com>
2024-03-20 23:01:50 -0300

    CI/CD Initial (#2) (#6)
    * Add Dependabot configuration and CI workflows***
    
    * Add IaC files for Terraform configuration
    
    * Update .gitignore and IaC configuration***
    
    * Add Earthfile for build configuration
    
    ---------
    
    Co-authored-by: Lucas Arruda <zatesec@loatecs.com>

---
+++ IaC/modules/app/vars.tf
@@ -0,0 +1,53 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "database_username" {
+  description = "Username for the postgres DB user."
+  type        = string
+}
+
+variable "database_password" {
+  description = "password of the postgres database"
+  type        = string
+}
+
+variable "database_port" {
+  description = "port used by postgres database"
+  type        = number
+}
+
+variable "database_host" {
+  default = "Postgres database host's address"
+  type    = string
+}
+
+variable "database_name" {
+  default = "Name of postgres database"
+  type    = string
+}
+
+variable "image_registry" {
+  description = "The registry where the image is stored"
+  type        = string
+  default = "ghcr.io/soat1stackgolang"
+}
+
+variable "svc_hackaton_image_tag" {
+  description = "The tag of the image for the Hackaton service"
+  type        = string
+  default = "svc-develop"
+}
+
+variable "svc_hackaton_migs_image_tag" {
+  description = "The tag of the image for the Hackaton service"
+  type        = string
+  default = "migs-develop"
+}
+
+variable "redeploy_annotation" {
+  description = "Annotation to trigger a redeploy"
+  type        = string
+  default = "none"
+}
---
+++ IaC/verify.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+######################## IMPORTANTE ########################################################
+#  Antes de executar esse script, atualize os dados abaixo com as informações fornecidas pelo
+#  terraform apply
+############################################################################################
+apigw_endpoint="https://704qemtrt4.execute-api.us-east-1.amazonaws.com"
+cognito_client_id="6rvfop6n2r63u46kjc3n5mvc6b"
+cognito_url="https://hackaton-dev.auth.us-east-1.amazoncognito.com"
+cognito_userpool_id="us-east-1_aQoj4mSdM"
+
+
+cognito_username="11122233300"
+cognito_password="F@ap1234"
+
+
+token=$(aws cognito-idp admin-initiate-auth --user-pool-id $cognito_userpool_id \
+    --client-id $cognito_client_id \
+    --auth-flow ADMIN_NO_SRP_AUTH \
+    --auth-parameters USERNAME=$cognito_username,PASSWORD=$cognito_password \
+| jq -r '.AuthenticationResult.AccessToken')
+
+
+echo -e "\n-------------------------------TOKEN------------------------------------------"
+echo "$token"
+
+test_endpoint="$apigw_endpoint/category/9764bd96-3bcf-11ee-be56-0242ac120002"
+#test_endpoint="$apigw_endpoint/category/all"
+
+test_body='{limit: 10, offset: 0}'
+
+echo -e "\n-------------------------TEST WITHOUT TOKEN------------------------------------"
+
+test_without_token="curl --location $test_endpoint -s \
+--header 'Content-Type: application/json' \
+--data '$test_body'"
+
+eval "$test_without_token"
+
+
+echo -e "\n-------------------------TEST WITH TOKEN-------------------------------------"
+
+test_with_token="curl -X GET --location $test_endpoint -s \
+--header 'Authorization: $token'  \
+--header 'Content-Type: application/json' \
+--data '$test_body'"
+
+eval "$test_with_token"
+
+# Swagger URL
+echo -e "\n-------------------------SWAGGER URL-------------------------------------"
+echo -e "Hackaton Swagger URL: $apigw_endpoint/swagger/index.html"
+echo -e "Production Swagger URL: $apigw_endpoint/production/swagger/index.html"
+
+
