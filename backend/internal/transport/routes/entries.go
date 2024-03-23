93f2f4b4ede1e332c7e39a07f6630316128bfe70
Lucas O Arruda <lucas.o.arruda@live.com>
2024-03-23 00:41:45 -0300

    Feature/swagger-setup and JWT support (#16)
    * Remove terraform README.md file
    
    * Update Swagger API documentation
    
    * Update environment variable in main.tf
    
    * Update S3 bucket creation in aws-setup.sh
    
    * Update lambda.tf to use local bundle.zip
    
    * Update app module and variables***
    
    ---------
    
    Co-authored-by: Lucas Arruda <zatesec@loatecs.com>
    Co-authored-by: aluciade <aluciade2@gmail.com>

--- backend/internal/transport/routes/entries.go
+++ backend/internal/transport/routes/entries.go
@@ -3,13 +3,15 @@ package routes
 import (
 	"context"
 	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/SOAT1StackGoLang/Hackaton/internal/endpoints"
 	"github.com/SOAT1StackGoLang/Hackaton/internal/service"
 	kittransport "github.com/go-kit/kit/transport"
 	httptransport "github.com/go-kit/kit/transport/http"
 	kitlog "github.com/go-kit/log"
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 func NewEntriesRoutes(r *mux.Router, svc service.EntriesService, logger kitlog.Logger) *mux.Router {
@@ -31,11 +33,41 @@ func NewEntriesRoutes(r *mux.Router, svc service.EntriesService, logger kitlog.L
 
 }
 
+// CreateEntry godoc
+//
+// @Summary Create a new entry
+// @Tags Entries
+// @Security	ApiKeyAuth
+// @Accept  json
+// @Produce  json
+// @Param   user_id   header    string   false    "User ID"
+// @Param   entry_at  body    string   true    "Entry At"  SchemaExample({"entry_at": "2022-01-01T00:00:00Z"})
+// @Success 200 {string} string "ok"
+// @Failure 400 {string} string "error"
+// @Failure 500 {string} string "error"
+// @Router /entries [post]
 func decodeCreateEntryRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	// TODO extract userID
-	var request endpoints.InsertEntryRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	claims, err := getJWTTokenJSON(r)
+	if err != nil {
+		log.Println(err)
+	}
+
+	username, ok := claims["username"].(string)
+	if !ok {
+		username = r.Header.Get("user_id")
+		log.Println("Bad request: unable to find expected value. User ID from header:", username)
+	}
+
+	var requestBody struct {
+		EntryAt string `json:"entry_at"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
 		return nil, err
+	}
+
+	request := endpoints.InsertEntryRequest{
+		UserID:  username,
+		EntryAt: requestBody.EntryAt,
 	}
 
 	return request, nil
